1dd33b4f91ecad83cb3d744333c677ad2875954d
Teynur Yuseinov <teynur@teynurs-air.home>
2026-02-26 23:51:39 +0100

    face

--- scripts/face.js
+++ scripts/face.js
@@ -4,10 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentMask = 0;
 
   const faceMask = document.querySelector("#faceMask");
-
   if (!faceMask) return;
 
-  // Wissel masker bij klik / tap
   document.body.addEventListener("click", () => {
     currentMask = (currentMask + 1) % masks.length;
     faceMask.setAttribute("src", masks[currentMask]);
