d32a14550abba3744a3add91089298b1f708732f
Teynur Yuseinov <teynur@teynurs-air.home>
2026-02-26 12:36:48 +0100

    spider verse

--- scripts/image.js
+++ scripts/image.js
@@ -1,40 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const target = document.querySelector("#paulTarget");
-const paul = document.querySelector("#paul");
-const mist = document.querySelector("#mist");
+  const video = document.querySelector("#portalVideo")
+  const scene = document.querySelector("a-scene")
 
-let running=false;
-let t=0;
-let z=0;
+  if(!video || !scene) return
 
-target.addEventListener("targetFound",()=>{
-running=true;
+  const targets = document.querySelectorAll("[mindar-image-target]")
 
-mist.setAttribute("animation",{
-property:"material.opacity",
-to:0.1,
-dur:1200
-});
+  targets.forEach(target => {
 
-animateDepth();
-});
+    target.addEventListener("targetFound", () => {
+      video.currentTime = 0
+      video.play().catch(() => {})
+    })
 
-target.addEventListener("targetLost",()=>{
-running=false;
-mist.setAttribute("material","opacity",0);
-});
+    target.addEventListener("targetLost", () => {
+      video.pause()
+    })
 
-function animateDepth(){
-if(!running)return;
+  })
 
-t+=0.002;
-const targetZ=Math.sin(t)*0.02;
-z+=(targetZ-z)*0.04;
-
-paul.setAttribute("position",`0 0.02 ${0.18+z}`);
-
-requestAnimationFrame(animateDepth);
-}
-
-});
+})
